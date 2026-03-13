bdb053a49bb307211715d70073d0237a2e73eecd
Michael Hobart <michaellhobart@gmail.com>
2026-03-13 12:05:18 -0400

    feat: implement CLI entry point and init command

---
+++ src/cli.ts
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+import { Command } from 'commander';
+import { initCommand } from './commands/init.js';
+
+const program = new Command();
+
+program
+  .name('napkin')
+  .description('Turn architecture diagrams into runnable sketches')
+  .version('0.1.0');
+
+program.addCommand(initCommand);
+
+program.parse();
